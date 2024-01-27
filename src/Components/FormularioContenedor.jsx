ec2901a7aebf02fec9af4e4f8db80228daef8459
emir paradi <emir.alejandro.paradi@gmail.com>
2024-01-27 16:31:59 -0300

    Navbar y fotter agregados

--- src/Components/FormularioContenedor.jsx
+++ src/Components/FormularioContenedor.jsx
@@ -2,16 +2,11 @@ import { Form } from "react-bootstrap";
 import FormularioInputs from "./FormularioInputs";
 
 const Formulario = () => {
-
   return (
-    <>
-    <section
-      className="border border-primary rounded-1 bg-body-tertiary py-5 px-5"
-    >
+    <section className="border border-primary rounded-1 bg-body-tertiary py-5 px-5 my-5">
       <h1 className="text-secondary mb-5">Cargar Datos</h1>
       <FormularioInputs></FormularioInputs>
     </section>
-    </>
   );
 };
 
